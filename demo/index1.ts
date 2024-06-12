baa3174078e6f6f25952d9b9ee8451b887b4249b
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-06-12 15:33:00 -0700

    remove cjs

--- demo/index1.ts
+++ demo/index1.ts
@@ -2,10 +2,5 @@ import PDom from 'parallel-dom';
 
 const pdom = new PDom(
     '#parallel',
-    new URL('pivot.js', import.meta.url).href,
-    {
-        iframe: false,
-        isDomainShardingDisabled: isSelfHosted,
-        hostingDomain: (isSelfHosted) ? `${window.location.host}/pdom` : 'pdom.thoughtspot{env}.cloud'
-    }
+    new URL('worker.js', import.meta.url).href
 );
