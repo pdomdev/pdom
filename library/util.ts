e2e78c05ac5f54d5051b248d8ac61763c44a6858
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-06-21 18:15:20 -0700

    fix import regex

--- library/util.ts
+++ library/util.ts
@@ -1,5 +1,5 @@
 // extract the script url from a function which has a dynamic import
 // for the script url.
 export function getScriptUrlFromFunction(fn: () => Promise<any>) {
-    return new URL(fn.toString().match(/import\(["'](.+)["']\)/)[1], import.meta.url).href;
+    return new URL(fn.toString().match(/import\([\s\S]*["'](.+)["'][\s\S]*\)/)[1], import.meta.url).href;
 }
