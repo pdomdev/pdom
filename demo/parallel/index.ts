9e119e4848180082d032b43a2c32e2aa22143c6d
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-07-01 23:22:47 -0700

    bugfix

--- demo/parallel/index.ts
+++ demo/parallel/index.ts
@@ -4,8 +4,6 @@ import PDom from '../../library/index';
 
 const pdom = new PDom(
     '#container',
-    {
-        script: () => import('../common/busy'),
-    }
+    () => import('../common/busy'),
 );
 pdom.render();
