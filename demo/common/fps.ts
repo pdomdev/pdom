e11e42fe68bfda5d65d231362821657a9b1ac9e6
Ashish Shubham <ashish.shubham@thoughtspot.com>
2024-07-02 10:37:53 -0700

    fix stats

--- demo/common/fps.ts
+++ demo/common/fps.ts
@@ -2,23 +2,25 @@ import Stats from 'stats.js';
 
 const statsContainer = document.getElementById('stats');
 
-var fpsStats = new Stats();
-fpsStats.showPanel(0);
-fpsStats.dom.style.cssText = '';
-statsContainer.appendChild(fpsStats.dom);
+if (statsContainer) {
+    var fpsStats = new Stats();
+    fpsStats.showPanel(0);
+    fpsStats.dom.style.cssText = '';
+    statsContainer.appendChild(fpsStats.dom);
 
-var memStats = new Stats();
-memStats.showPanel(2);
-memStats.dom.style.cssText = '';
-statsContainer.appendChild(memStats.dom);
+    var memStats = new Stats();
+    memStats.showPanel(2);
+    memStats.dom.style.cssText = '';
+    statsContainer.appendChild(memStats.dom);
 
-var msStats = new Stats();
-msStats.showPanel(1);
-msStats.dom.style.cssText = '';
-statsContainer.appendChild(msStats.dom);
+    var msStats = new Stats();
+    msStats.showPanel(1);
+    msStats.dom.style.cssText = '';
+    statsContainer.appendChild(msStats.dom);
 
-requestAnimationFrame(function loop() {
-    fpsStats.update();
-    memStats.update();
-    requestAnimationFrame(loop)
-});
+    requestAnimationFrame(function loop() {
+        fpsStats.update();
+        memStats.update();
+        requestAnimationFrame(loop)
+    });
+}
